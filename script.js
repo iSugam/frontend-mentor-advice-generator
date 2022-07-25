8014a99275ecd356042ccf84498469cce31faef3
unknown <sugamanandsahu01@gmail.com>
2022-07-26 03:15:28 +0530

    Minor changes.

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-let adviceID = document.querySelector(".id-num")
-let adviceText = document.getElementById("text")
+let adviceID = document.querySelector(".id-num");
+let adviceText = document.getElementById("text");
 
 const getAdvice = async () => {
     try {
-        const response = await fetch("https://api.adviceslip.com/advice", {method: "GET"})
-        const result = await response.json()
-        console.log(result.slip);
+        const response = await fetch("https://api.adviceslip.com/advice", {method: "GET"});
+        const result = await response.json();
 
-        adviceID.innerText = `#${result.slip.id}`
+        adviceID.innerText = `#${result.slip.id}`;
         adviceText.innerText = `"${result.slip.advice}"`;
     } catch(err) {
-        adviceText.innerText = err.message
+        adviceText.innerText = err.message;
     }
 }
 
-getAdvice()
+getAdvice();
